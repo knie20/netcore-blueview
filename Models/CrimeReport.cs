4a8c33e03e9636fffaca35d07ea80e950f9eb977
tspao <tspao@DESKTOP-TP46VHN>
2018-10-27 19:17:58 -0400

    Added CrimeReport Model

---
+++ Models/CrimeReport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netcore_blueview.Models
+{
+    public class CrimeReport
+    {
+        public int ReportID { get; set; }
+        public DateTime StartTime { get; set; }
+        public string CrimeCode { get; set; }
+        public string Location { get; set; }
+        public string AudioUrl { get; set; }
+        public string Transcript { get; set; }
+    }
+}
