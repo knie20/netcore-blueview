e5d3f9b35d23a7093a6cd8eb463b08689e3b3ad9
tspao <tspao@DESKTOP-TP46VHN>
2018-10-27 19:01:55 -0400

    Made a couple controller bois I guess

---
+++ Services/SpeechRecognitionsService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netcore_blueview.Services
+{
+    public class SpeechRecognitionsService
+    {
+        public void addSpeech() {
+           
+        }
+    }
+}
