f6862805e514478594805b20e8421390d6dd2179
Kaidong Nie <niekg@mail.uc.edu>
2018-10-27 22:54:21 -0400

    rebuilt models.

---
+++ Models/Alternative.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace netcore_blueview.Models
+{
+    public class Alternative
+    {
+        public int AlternativeId { get; set; }
+
+        public string Transcript { get; set; }
+        private float Confidence { get; set; }
+
+        public int SpeechRecognitionResultId { get; set; }
+        public SpeechRecognitionResult SpeechRecognitionResult { get; set; }
+
+        public List<WordInfo> WordInfos { get; set; }
+    }
+}
---
+++ Models/SpeechRecognitionResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace netcore_blueview.Models
+{
+    public class SpeechRecognitionResponse
+    {
+        public int SpeechRecognitionResponseId { get; set; }
+
+        public string AudioUrl { get; set; }
+        public DateTime StartTime { get; set; }
+
+        public List<SpeechRecognitionResult> SpeechRecognitionResults { get; set; }
+    }
+}
